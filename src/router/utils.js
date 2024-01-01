ebcf4d6b3022ea63562be0a448b0928994fd97d1
임빛나 <devbit4@gmail.com>
2024-01-01 12:26:06 +0900

    feat : 페이지 라우팅 기능 추가

---
+++ src/router/utils.js
@@ -0,0 +1,11 @@
+export const pathToRegex = (path) =>
+  new RegExp(`^${path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)')}$`);
+
+export const getParams = (match) => {
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1],
+  );
+  const values = match.result.slice(1);
+
+  return Object.fromEntries(keys.map((key, index) => [key, values[index]]));
+};
