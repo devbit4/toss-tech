b157797229839a596bc5cbeaf4a175a4bc698cc8
임빛나 <devbit4@gmail.com>
2024-01-08 00:58:04 +0900

    refactor : router 변수명 수정 및 routes 파일 분리

--- src/router/utils.js
+++ src/router/utils.js
@@ -1,11 +1,12 @@
 export const pathToRegex = (path) =>
   new RegExp(`^${path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)')}$`);
 
-export const getParams = (match) => {
-  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
-    (result) => result[1],
+export const getParams = (target) => {
+  const keys = Array.from(target.route.path.matchAll(/:(\w+)/g)).map(
+    (paramsInfo) => paramsInfo[1],
   );
-  const values = match.result.slice(1);
+
+  const values = target.matchedPathInfo.slice(1);
 
   return Object.fromEntries(keys.map((key, index) => [key, values[index]]));
 };
