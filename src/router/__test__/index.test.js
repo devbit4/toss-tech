e09d2ba2c6910b7754b24d43e6b526300934a82b
임빛나 <devbit4@gmail.com>
2024-01-12 23:04:12 +0900

    fix : 라우터 클래스 수정 및 라우터 테스트 코드 수정

--- src/router/__test__/index.test.js
+++ src/router/__test__/index.test.js
@@ -1,27 +1,69 @@
-/* eslint-disable no-undef */
-const { findRoute } = require('../index');
+const { default: Component } = require('../../core/Component');
+const { default: Router } = require('..');
 const { pathToRegex, getParams, getQueryParams } = require('../utils');
 
-const routes = [
-  {
-    path: '/',
-    page: jest.fn().mockReturnValue('<div>main-page</div>'),
-  },
-  { path: '/posts', page: jest.fn().mockReturnValue('<div>list-page</div>') },
-  {
-    path: '/posts/:id',
-    page: jest.fn().mockReturnValue('<div>detail-page</div>'),
-  },
-  { path: '*', page: jest.fn().mockReturnValue('<div>404-page</div>') },
-];
+class Home extends Component {
+  template() {
+    return `<h3>home</h3>`;
+  }
+}
+
+class Posts extends Component {
+  template() {
+    return `<h3>list</h3>`;
+  }
+}
+
+class Post extends Component {
+  template() {
+    return `<h3>detail</h3>`;
+  }
+}
+
+class NotFound extends Component {
+  template() {
+    return `<h3>404</h3>`;
+  }
+}
 
 describe('router test', () => {
+  const target = document.body;
+  const router = new Router(target);
+
+  beforeAll(() => {
+    router
+      .addRoute({
+        path: '/',
+        page: Home,
+      })
+      .addRoute({
+        path: '/posts',
+        page: Posts,
+      })
+      .addRoute({
+        path: '/posts/:id',
+        page: Post,
+      })
+      .addRoute({
+        path: '*',
+        page: NotFound,
+      });
+  });
+
+  test('should verify that the addRoute function works properly', () => {
+    expect(router.routes.length).toBe(4);
+    expect(router.routes.length).not.toBe(3);
+    expect(router.routes[0].path).toBe('/');
+    expect(router.routes[0].path).not.toBe('/posts');
+    expect(router.routes[0].path).not.toBe('/hello');
+  });
+
   test('should verify that the pathToRegex function works properly', () => {
-    const regex = pathToRegex('/');
+    const regex = pathToRegex('/posts');
     const regex2 = pathToRegex('/posts/:id');
 
-    expect(regex.test('/')).toBeTruthy();
-    expect(regex.test('/posts')).toBeFalsy();
+    expect(regex.test('/posts')).toBeTruthy();
+    expect(regex.test('/posts/3')).toBeFalsy();
 
     expect(regex2.test('/posts/3')).toBeTruthy();
     expect(regex2.test('/posts')).toBeFalsy();
@@ -29,7 +71,8 @@ describe('router test', () => {
 
   test('should return correct params when the getParams function works properly', () => {
     const pathname = '/posts/1';
-    const targetRoute = findRoute(routes, pathname);
+    const targetRoute = router._findRoute(pathname);
+
     expect(getParams(targetRoute)).toEqual({ id: '1' });
     expect(getParams(targetRoute).id).not.toBe(2);
     expect(getParams(targetRoute)).not.toEqual({ no: '1' });
@@ -45,38 +88,27 @@ describe('router test', () => {
   });
 
   test('should verify that the findRoute function works properly', () => {
-    let matchedRoutePah = findRoute(routes, '/').route.path;
-    expect(matchedRoutePah).toBe('/');
-    expect(matchedRoutePah).not.toBe('*');
-
-    matchedRoutePah = findRoute(routes, '/posts/3').route.path;
+    let matchedRoutePah = router._findRoute('/posts/3').route.path;
     expect(matchedRoutePah).toBe('/posts/:id');
     expect(matchedRoutePah).not.toBe('*');
 
-    matchedRoutePah = findRoute(routes, '/404').route.path;
+    matchedRoutePah = router._findRoute('/404').route.path;
     expect(matchedRoutePah).toBe('*');
     expect(matchedRoutePah).not.toBe('/posts/:id');
 
-    matchedRoutePah = findRoute(routes, '/hello/3').route.path;
+    matchedRoutePah = router._findRoute('/hello/3').route.path;
     expect(matchedRoutePah).toBe('*');
     expect(matchedRoutePah).not.toBe('/posts/:id');
   });
 
-  test('should verify that the matchedRoutePage is rendered properly', () => {
-    const matchedRoute = findRoute(routes, '/posts/3');
+  test('should render the target page when the navigateTo function works properly', () => {
+    router._navigateTo('/posts');
 
-    const detailPageRoute = routes.find((route) => route.path === '/posts/:id');
-    const listPageRoute = routes.find((route) => route.path === '/posts');
-    const notFoundPageRoute = routes.find((route) => route.path === '*');
+    expect(target.innerHTML).toContain('<h3>list</h3>');
+    expect(target.innerHTML).not.toContain('<h3>detail</h3>');
 
-    document.body.innerHTML = matchedRoute.route.page();
-
-    expect(detailPageRoute.page).toHaveBeenCalled();
-    expect(listPageRoute.page).not.toHaveBeenCalled();
-    expect(notFoundPageRoute.page).not.toHaveBeenCalled();
-
-    expect(document.body.innerHTML).toContain(detailPageRoute.page());
-    expect(document.body.innerHTML).not.toContain(listPageRoute.page());
-    expect(document.body.innerHTML).not.toContain(notFoundPageRoute.page());
+    router._navigateTo('/hello');
+    expect(target.innerHTML).not.toContain('<h3>list</h3>');
+    expect(target.innerHTML).toContain('<h3>404</h3>');
   });
 });
