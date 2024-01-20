656f3adb17427c940676fd232718e94fa9cb6f7a
임빛나 <devbit4@gmail.com>
2024-01-20 20:33:43 +0900

    feat : Articles, Article, Design, NotFound 페이지 퍼블리싱 반영

--- src/pages/Design/index.js
+++ src/pages/Design/index.js
@@ -1,8 +1,16 @@
 import Component from '@/core/Component';
 
+import styles from './styles.module.css';
+
 class DesignPage extends Component {
   template() {
-    return `<h3>디자인 페이지입니다.</h3>`;
+    return `
+    <div class=${styles.page__container}>
+      <div class=${styles.page__inner}>
+        <h3 class=${styles.page__title}>디자인</h3>
+        <div class=${styles.design__list}>디자인 페이지입니다.</div>
+      </div>
+    </div>`;
   }
 }
 
--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,8 +1,16 @@
 import Component from '@/core/Component';
 
+import styles from './styles.module.css';
+
 class NotFoundPage extends Component {
   template() {
-    return `<h3>이 페이지를 찾을 수 없습니다.</h3>`;
+    return `
+    <div class=${styles.page__container}>
+      <div class=${styles.page__inner}>
+        <h3 class=${styles.page__title}>404</h3>
+        <div class=${styles.text}>이 페이지를 찾을 수 없습니다.</div>
+      </div>
+    </div>`;
   }
 }
 
